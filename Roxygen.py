95786ccc38d895f8723f91aa338b0dcbc3823afa
Karthik Ram <karthik.ram@gmail.com>
2012-02-27 21:22:41 -0800

    Fixed an error that resulted in a backtick instead of a single quote.

--- Roxygen.py
+++ Roxygen.py
@@ -8,11 +8,11 @@ class RDocsCommand(sublime_plugin.TextCommand):
       params_txt = self.view.substr(params_reg)
       params = params_txt.split(',')
 
-      snippet = "#`<brief desc>\n#`\n#`<full description>\n"
+      snippet = "#'<brief desc>\n#'\n#'<full description>\n"
 
       for p in params:
-         snippet += "#`@param %s <what param does>\n" % p
+         snippet += "#'@param %s <what param does>\n" % p
 
-      snippet+= "#`@keywords\n#`@seealso\n#`@return\n#`@alias\n#`@export\n#`@examples \dontrun{\n#`\n#` }"
+      snippet+= "#'@keywords\n#'@seealso\n#'@return\n#'@alias\n#'@export\n#'@examples \dontrun{\n#'\n#'}"
 
       self.view.insert(edit, sel.begin(), snippet)

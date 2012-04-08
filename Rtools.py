62db0815915aab046fc6453cb8850c6e4a8d3152
Winston Chang <winston@stdout.org>
2012-04-08 14:49:28 -0500

    Make 'run' work when nothing is selected

--- Rtools.py
+++ Rtools.py
@@ -34,7 +34,12 @@ class SendSelectionCommand(sublime_plugin.TextCommand):
         # get selection
         selection = ""
         for region in self.view.sel():
-            selection += self.view.substr(region) + "\n"
+            if region.empty():
+                selection += self.view.substr(self.view.line(region)) + "\n"
+                self.advanceCursor(region)
+            else:
+                selection += self.view.substr(region) + "\n"
+
         selection = (selection[::-1].replace('\n'[::-1], '', 1))[::-1]
 
         # only proceed if selection is not empty
@@ -57,3 +62,18 @@ class SendSelectionCommand(sublime_plugin.TextCommand):
             args.extend(['-e', 'tell app "Sublime Text 2" to activate'])
             # execute code
             subprocess.Popen(args)
+
+
+    def advanceCursor(self, region):
+        (row, col) = self.view.rowcol(region.begin())
+
+        # Make sure not to go past end of next line
+        nextline = self.view.line(self.view.text_point(row + 1, 0))
+        if nextline.size() < col:
+            loc = self.view.text_point(row + 1, nextline.size())
+        else:
+            loc = self.view.text_point(row + 1, col)
+
+        # Remove the old region and add the new one
+        self.view.sel().subtract(region)
+        self.view.sel().add(sublime.Region(loc, loc))
